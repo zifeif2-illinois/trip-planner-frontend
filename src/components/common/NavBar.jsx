4771373afa762c21ef8865fcb5b69fadc1ad8340
zfrandom <zfrandomstuff@yahoo.com>
2019-03-10 00:28:27 -0600

    deploy

--- src/components/common/NavBar.jsx
+++ src/components/common/NavBar.jsx
@@ -7,15 +7,15 @@ export default class NavBar extends Component {
   render() {
     return (
       <Menu pointing secondary>
-        <Link to='/home'>
+        <Link to='/sp19-cs498rk-mp2/home'>
           <Menu.Item active={this.props.active==='home'} name='home'>
           </Menu.Item>
         </Link>
-        <Link to="/gallery">
+        <Link to="/sp19-cs498rk-mp2/gallery">
           <Menu.Item active={this.props.active==='gallery'} name='gallery'>
           </Menu.Item>
         </Link>
-        <Link to='/list'>
+        <Link to='/sp19-cs498rk-mp2/list'>
           <Menu.Item active={this.props.active==='list'} name='list'>
           </Menu.Item>
         </Link>
