ce9678ce9cd2450f68d2f2919c45fb1428a1669f
zfrandom <zfrandomstuff@yahoo.com>
2019-04-29 21:27:08 -0500

    add city search bar

--- src/components/common/SelectionButtons.js
+++ src/components/common/SelectionButtons.js
@@ -4,9 +4,9 @@ import {Button} from 'semantic-ui-react'
 export default function SelectionButtons(props) {
   return (
     <Button.Group>
-      <Button color={props.selectOption1?'teal':''} onClick={props.onToggle}>{props.option1}</Button>
+      <Button color={props.selectOption1?'teal':null} onClick={props.onToggle}>{props.option1}</Button>
       <Button.Or />
-      <Button color={!props.selectOption1?'teal':''} onClick={props.onToggle}>{props.option2}</Button>
+      <Button color={!props.selectOption1?'teal':null} onClick={props.onToggle}>{props.option2}</Button>
     </Button.Group>
   )
 }
