0584390ed234e032b61ac9b50461ae91d8444ee7
Tianyu Sun <tianyus2@illinois.edu>
2019-04-30 02:32:45 -0500

    Search city activities

--- src/components/Create/SearchView.js
+++ src/components/Create/SearchView.js
@@ -1,15 +1,17 @@
-import React, {Component} from 'react'
-import { Card, Button, Image} from 'semantic-ui-react'
-import '../../style/SearchView.scss'
+import React, {Component} from 'react';
+import { Card, Button, Image, List } from 'semantic-ui-react';
+import Script from 'react-load-script';
+import {GOOGLE_PLACES_API_KEY} from '../../config.js';
+import '../../style/SearchView.scss';
 
 class SearchResultCard extends Component {
   render() {
     return (
       <Card className='search-result'>
         <Card.Content>
-          <Image floated='right' size='mini' src='https://react.semantic-ui.com/images/avatar/large/steve.jpg' />
+          <Image floated='right' size='mini' src={this.props.result.photos[0].getUrl({maxWidth: 400, maxHeight: 400})}/>
           <Card.Header>{this.props.result.name}</Card.Header>
-          <Card.Meta>Location: {this.props.result.location}</Card.Meta>
+          <Card.Meta>Location: {this.props.result.formatted_address}</Card.Meta>
           <Button onClick={()=>this.addToBoard(this.props.result)}> Add </Button>
         </Card.Content>
       </Card>)
@@ -18,21 +20,49 @@ class SearchResultCard extends Component {
 
 export default class SearchView extends Component {
   constructor(props) {
-    super(props)
+    super(props);
+    this.state = {
+      results: []
+    };
+
+    this.handleScriptLoad = this.handleScriptLoad.bind(this);
+  }
+
+  handleScriptLoad() {
+    /*global google*/
+    const service = new google.maps.places.PlacesService(document.getElementById('map'));
+
+    const city = this.props.city || 'Chicago';
+    const query_city = city.toLowerCase();
+    const request = {
+      query: `${query_city} points of interest`
+    };
+
+    service.textSearch(request, (results, status) => {
+      if (status == google.maps.places.PlacesServiceStatus.OK) {
+        // for (var i = 0; i < results.length; i++) {
+        //   var place = results[i];
+        //   createMarker(results[i]);
+        // }
+        this.setState({results: results})
+      }
+    });
   }
 
   render() {
-    return (<div className='search-view-container'>
-    <SearchResultCard result={{name: 'Japan house', location: '2110 S White Street'}}/>
-    <SearchResultCard result={{name: 'Japan house', location: '2110 S White Street'}}/>
-    <SearchResultCard result={{name: 'Japan house', location: '2110 S White Street'}}/>
-    <SearchResultCard result={{name: 'Japan house', location: '2110 S White Street'}}/>
-    <SearchResultCard result={{name: 'Japan house', location: '2110 S White Street'}}/>
-        <SearchResultCard result={{name: 'Japan house', location: '2110 S White Street'}}/>
-        <SearchResultCard result={{name: 'Japan house', location: '2110 S White Street'}}/>
-        <SearchResultCard result={{name: 'Japan house', location: '2110 S White Street'}}/>
-        <SearchResultCard result={{name: 'Japan house', location: '2110 S White Street'}}/>
-        <SearchResultCard result={{name: 'Japan house', location: '2110 S White Street'}}/>
-    </div>)
+    return (
+      <div className='search-view'>
+        <Script
+          url={`https://maps.googleapis.com/maps/api/js?key=${GOOGLE_PLACES_API_KEY}&libraries=places`}
+          onLoad={this.handleScriptLoad}
+        />
+        <List className='search-view-container'>
+          {this.state.results.map(result =>
+            <SearchResultCard result={result}/>
+          )}
+        </List>
+        <div id='map'></div>
+      </div>
+    );
   }
 }
