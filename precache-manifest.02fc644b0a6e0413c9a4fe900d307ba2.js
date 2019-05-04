a84dd7b01f2567f8acb6b91b062521bc665f678e
Tianyu Sun <tianyus2@illinois.edu>
2019-05-03 22:51:05 -0500

    Updates

---
+++ precache-manifest.02fc644b0a6e0413c9a4fe900d307ba2.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/cs498rk-project/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "18cc3e83b5ccfa3d7be9",
+    "url": "/cs498rk-project/static/css/main.6f53e91a.chunk.css"
+  },
+  {
+    "revision": "e3b9368cd60f99e274d4",
+    "url": "/cs498rk-project/static/js/runtime~main.e3b9368c.js"
+  },
+  {
+    "revision": "d0ed1b087250eddf7b741340806b66a8",
+    "url": "/cs498rk-project/static/media/review-background.d0ed1b08.jpg"
+  },
+  {
+    "revision": "0d5ce921c4f104e2c881",
+    "url": "/cs498rk-project/static/js/2.0d5ce921.chunk.js"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/cs498rk-project/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/cs498rk-project/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/cs498rk-project/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/cs498rk-project/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/cs498rk-project/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/cs498rk-project/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/cs498rk-project/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "18cc3e83b5ccfa3d7be9",
+    "url": "/cs498rk-project/static/js/main.18cc3e83.chunk.js"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/cs498rk-project/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/cs498rk-project/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/cs498rk-project/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/cs498rk-project/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/cs498rk-project/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/cs498rk-project/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/cs498rk-project/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/cs498rk-project/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "e58f402ccc801cd5a37599aef3f27915",
+    "url": "/cs498rk-project/static/media/home-background.e58f402c.jpg"
+  },
+  {
+    "revision": "0d5ce921c4f104e2c881",
+    "url": "/cs498rk-project/static/css/2.ab40bffb.chunk.css"
+  },
+  {
+    "revision": "ff05c011b4dd1ae29ac2075931c59471",
+    "url": "/cs498rk-project/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/cs498rk-project/precache-manifest.02fc644b0a6e0413c9a4fe900d307ba2.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/cs498rk-project/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
