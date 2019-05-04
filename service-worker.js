2329dc657f098f796734a71d6e488451808aff15
Tianyu Sun <tianyus2@illinois.edu>
2019-05-03 22:56:28 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/cs498rk-project/precache-manifest.02fc644b0a6e0413c9a4fe900d307ba2.js"
+  "/trip-planner-frontend/precache-manifest.8f7c1841368d9f362e5dc246cb668ecf.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/cs498rk-project/index.html", {
+workbox.routing.registerNavigationRoute("/trip-planner-frontend/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
